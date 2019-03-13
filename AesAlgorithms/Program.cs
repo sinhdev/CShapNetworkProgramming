c07e6d1a740ed8653461ce0962b1903676f382b5
Sinh NX <sinhnx@Sinhs-MacBook-Pro.local>
2019-03-13 17:05:25 +0700

    add AES (Advanced Encryption Standard) algorithms

---
+++ AesAlgorithms/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AesAlgorithms
+{
+    class Program
+    {
+        public static class Global
+        {
+            // set password
+            public const string strPassword = "LetMeIn99$";
+
+            // set permutations
+            public const String strPermutation = "ouiveyxaqtd";
+            public const Int32 bytePermutation1 = 0x19;
+            public const Int32 bytePermutation2 = 0x59;
+            public const Int32 bytePermutation3 = 0x17;
+            public const Int32 bytePermutation4 = 0x41;
+        }
+
+
+        // The console window
+        public static void Main(String[] args)
+        {
+            Console.Title = "Secure Password v2";
+
+            Console.WriteLine("Password:  " + Global.strPassword);
+
+            string strEncrypted = (Encrypt(Global.strPassword));
+            Console.WriteLine("Encrypted: " + strEncrypted);
+
+            string strDecrypted = (Decrypt(strEncrypted));
+            Console.WriteLine("Decrypted: " + strDecrypted);
+        }
+        // encoding
+        public static string Encrypt(string strData)
+        {
+            return Convert.ToBase64String(Encrypt(Encoding.UTF8.GetBytes(strData)));
+        }
+        // decoding
+        public static string Decrypt(string strData)
+        {
+            return Encoding.UTF8.GetString(Decrypt(Convert.FromBase64String(strData)));
+        }
+        // encrypt
+        public static byte[] Encrypt(byte[] strData)
+        {
+            PasswordDeriveBytes passbytes =
+            new PasswordDeriveBytes(Global.strPermutation,
+            new byte[] { Global.bytePermutation1,
+                         Global.bytePermutation2,
+                         Global.bytePermutation3,
+                         Global.bytePermutation4
+            });
+
+            MemoryStream memstream = new MemoryStream();
+            Aes aes = new AesManaged();
+            aes.Key = passbytes.GetBytes(aes.KeySize / 8);
+            aes.IV = passbytes.GetBytes(aes.BlockSize / 8);
+
+            CryptoStream cryptostream = new CryptoStream(memstream,
+            aes.CreateEncryptor(), CryptoStreamMode.Write);
+            cryptostream.Write(strData, 0, strData.Length);
+            cryptostream.Close();
+            return memstream.ToArray();
+        }
+
+        // decrypt
+        public static byte[] Decrypt(byte[] strData)
+        {
+            PasswordDeriveBytes passbytes =
+            new PasswordDeriveBytes(Global.strPermutation,
+            new byte[] { Global.bytePermutation1,
+                         Global.bytePermutation2,
+                         Global.bytePermutation3,
+                         Global.bytePermutation4
+            });
+
+            MemoryStream memstream = new MemoryStream();
+            Aes aes = new AesManaged();
+            aes.Key = passbytes.GetBytes(aes.KeySize / 8);
+            aes.IV = passbytes.GetBytes(aes.BlockSize / 8);
+
+            CryptoStream cryptostream = new CryptoStream(memstream,
+            aes.CreateDecryptor(), CryptoStreamMode.Write);
+            cryptostream.Write(strData, 0, strData.Length);
+            cryptostream.Close();
+            return memstream.ToArray();
+        }
+    }
+}
